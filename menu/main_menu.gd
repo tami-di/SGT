ff1079e39ac4cd56a3cad986f81e706893b91925
scarleth-bc <scarleth.betancurt@ug.uchile.cl>
2023-04-06 21:09:50 -0400

    construcción de menu inicio y pausa

---
+++ menu/main_menu.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+@onready var play = %Play
+@onready var settings = %Settings
+@onready var exit = %Exit
+
+func _ready():
+	play.pressed.connect(_on_play_pressed)
+	settings.pressed.connect(_on_settings_pressed)
+	exit.pressed.connect(_on_exit_pressed)
+	
+func _on_play_pressed():
+	#en res:blabla va el nombre de la escena del juego
+	#get_tree().change_scene_to_file("res://scenes/main.tscn")
+	pass
+
+func _on_settings_pressed():
+	pass
+	
+func _on_exit_pressed():
+	get_tree().quit()
