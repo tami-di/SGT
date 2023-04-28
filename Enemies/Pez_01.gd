70d68b7c51c5675a84897130cfadd94a66ed2dfb
Joyo-G <gustavo.joyo.p@gmail.com>
2023-04-28 03:49:18 -0400

    Modificación de enemigos y agregación de Muros

--- Enemies/Pez_01.gd
+++ Enemies/Pez_01.gd
@@ -1,24 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var SPEED = -110.0
 const JUMP_VELOCITY = -400.0
 var screen_size
 var switch = 0
-var k = 1
+@onready var sprite_2d = $Sprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimationPlayer.play("movement") #Hacer referencia para más tarde
+	velocity.x = SPEED
 
+func flip():
+	sprite_2d.flip_h = !sprite_2d.flip_h
+	velocity.x *= -1
 
 func _physics_process(delta):
-	position.x-=k
-	print(position)
-	if position.x <= 0:
-		$Sprite2D.flip_h = true
-		k= k*-1
-	if position.x >= 512:
-		$Sprite2D.flip_h = false
-		k= k*-1
-	$AnimationPlayer.play("movement")
 	move_and_slide()
--- main.gd
+++ main.gd
@@ -1,20 +1,19 @@
 extends Node2D
-
-@onready var camera = $Camera2D2
-@onready var camerazoom = $Camera2D
+@onready var markerAgua = $Marker2D
+@onready var player = $Player
+#@onready var camera = $Camera2D2
+#@onready var camerazoom = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Inicializar ambas cámaras como visibles
-	camera.enabled = true
-	camerazoom.enabled = false
-
+	#camera.enabled = true
+	#camerazoom.enabled = false
+	player.alturaNivelAgua = markerAgua.position.y
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
-		
 func _input(event):
-	if event.is_action_pressed("camara"):
-		camera.enabled = !camera.enabled
-		camerazoom.enabled = !camerazoom.enabled
+	#if event.is_action_pressed("camara"):
+	#	camera.enabled = !camera.enabled
+	#	camerazoom.enabled = !camerazoom.enabled
+	pass
