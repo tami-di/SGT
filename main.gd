bb93255a111a3540de74486be22844eea7de4560
Joyo-G <gustavo.joyo.p@gmail.com>
2023-04-28 18:26:34 -0400

    Cambios player y camara

--- main.gd
+++ main.gd
@@ -1,19 +1,19 @@
 extends Node2D
 @onready var markerAgua = $Marker2D
 @onready var player = $Player
-#@onready var camera = $Camera2D2
-#@onready var camerazoom = $Camera2D
+@onready var camerazoom = $Player/Camera2D
+@onready var camera = $Camera2D2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Inicializar ambas cámaras como visibles
-	#camera.enabled = true
-	#camerazoom.enabled = false
+	camera.enabled = true
+	camerazoom.enabled = false
 	player.alturaNivelAgua = markerAgua.position.y
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _input(event):
-	#if event.is_action_pressed("camara"):
-	#	camera.enabled = !camera.enabled
-	#	camerazoom.enabled = !camerazoom.enabled
-	pass
+	if event.is_action_pressed("camara"):
+		camera.enabled = !camera.enabled
+		camerazoom.enabled = !camerazoom.enabled
