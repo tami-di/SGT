cd653578c1b068e85e3a313ae52d7a2f4bf9c276
tami-di <111888329+tami-di@users.noreply.github.com>
2023-05-21 12:50:54 -0400

    lo nuevo

--- player/player.gd
+++ player/player.gd
@@ -100,17 +100,11 @@ func _physics_process(delta):
 	move_and_slide()
 	if Input.is_action_just_pressed("attack") and not inBoat:
 		_attack()
-<<<<<<< HEAD
 	
 	if Input.is_action_just_pressed("fish") and inBoat and not fishing and not inMovement: 
-=======
+		_fish()
 	if Input.is_action_just_pressed("attack_2"):
 		_attack_2()
-	
-	if Input.is_action_pressed("fish") and inBoat: 
->>>>>>> 5b4203e5406ec23fe5a3c34da3ad634572ce8eea
-		_fish()
-		
 	
 	if position.y > alturaNivelAgua:
 		sumergir()
