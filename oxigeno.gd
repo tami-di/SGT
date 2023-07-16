8d676e03617b1585accbf037bcaf81ddfe0d77dc
scarleth-bc <scarleth.betancurt@ug.uchile.cl>
2023-07-16 00:11:54 -0400

    oxigeno, game over base

---
+++ oxigeno.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+@onready var oxigen = $oxigen
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func set_sumergido(value):
+	#if isSumergido:
+	if oxigen.value > 0:
+		oxigen.value -= value
+	
+func set_oxigeno_actual():
+	if oxigen.value==0.0:
+		return -1
+	if oxigen.value==100:
+		return -2
