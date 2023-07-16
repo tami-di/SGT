56bdb212afe4c1591f7d034df316ea5397bded8d
Joyo-G <gustavo.joyo.p@gmail.com>
2023-07-16 13:39:12 -0400

    animation in game over and peces

--- game_over.gd
+++ game_over.gd
@@ -2,14 +2,20 @@ extends MarginContainer
 
 @onready var exit = %exit
 @onready var retry = %retry
-
+@onready var animation_player = $PanelContainer/MarginContainer/VBoxContainer/AnimationPlayer
+var i = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	retry.pressed.connect(_on_retry_pressed)
 	exit.pressed.connect(_on_exit_pressed)
 	hide()
-
+func _process(delta):
+	if visible and  i==0:
+		animation_player.play("new_animation")
+		i+=1
+	
+	
 func _on_retry_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
 	
